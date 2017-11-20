c2f78f913d4a1b8148c04161685f84a4fb16f5f4
Nolan Mayersky <nolan@northstarcreative.co>
2017-11-20 00:36:44 -0600

    Update bot.py
    overhauled. removed need for extra file 'votelist.txt'. tips can now be included or excluded. the bot now also comments after upvoting as well.

--- bot.py
+++ bot.py
@@ -1,62 +1,65 @@
-from steem.steem import Steem
-from steem.steem import BroadcastingError
+from piston.steem import Steem
+from piston.steem import BroadcastingError
 import threading
 import time
 import random
-import csv
- 
-my_subscriptions = []
- 
-with open('votelist.txt', mode='r') as infile:
-    reader = csv.reader(infile)
-    for rows in reader:
-        v = rows[0]
-        my_subscriptions.append(v)
-        
-        # accounts and passwords
-account = ["account_goes_here"]
-posting_key = ["password_goes_here"]
-# delay in seconds when the bot votes
-vote_delay = random.randrange(1200,1800)
+
+# vote_list is regular favorites get a .001 sbd tip
+vote_list = ["bitboss",  "user2", "user3", "etc"]
+my_favorites = ["noly"]
+
+my_subscriptions = top_writers + my_favorites
+
+#put your details here only put the active key if you want to tip people in your favorites list
+account = ""
+posting_key = ""
+active_key = ""
+#the number of seconds to wait after a post is published before you vote 1800 seconds = 30 minutes
+vote_delay = 1800
 
 upvote_history = []
- 
+
 def feed():
-    print("Waiting for new posts by %s\n\n\nGo Oprah!\nGo Winfrey!" % my_subscriptions)
-    steem = Steem(wif=posting_key[0])
+    print("Waiting for new posts by %s\n" % my_subscriptions)
+    steem = Steem(wif=posting_key, node="wss://node.steem.ws")
     for comment in steem.stream_comments():
- 
+
         if comment.author in my_subscriptions:
-            # Comments don't have titles. This is how we can know if we have a post or a comment.
             if len(comment.title) > 0:
- 
-                # check if we already upvoted this. Sometimes the feed will give duplicates.
+
+                # check if we already upvoted this.
                 if comment.identifier in upvote_history:
                     continue
- 
+
                 print("New post by @%s %s" % (comment.author, url_builder(comment)))
                 workerThread = threading.Thread(name=comment.identifier, target=worker, args=(comment,))
                 workerThread.start()
- 
+
+# tip the best guys
+def send_a_tip(author):
+    steem = Steem(wif=active_key)
+    steem.transfer(author, 0.001, "SBD", memo="Here is a small gift for you.", account=account)
 
 def url_builder(comment):
     return "https://steemit.com/%s/%s" % (comment.category, comment.identifier)
- 
+
 def worker(worker_comment):
      time.sleep(vote_delay)
      try:
-       for (k,v) in enumerate(account):
-         worker_steem = Steem(wif=posting_key[k])
-         upvote_comment = worker_steem.get_content(worker_comment.identifier)
-         # vote weight 100 full power vote -100 full power flag
-         upvote_comment.vote(100, v)
-         print("@%s====> ^Upvoted^" % upvote_comment.author)
-         upvote_history.append(upvote_comment.identifier)
+       worker_comment.vote(50, account)
+       print("====> Upvoted")
+       worker_comment.reply('You have been upvoted by Prosper-ai! Enjoy!')
+       upvote_history.append(worker_comment.identifier)
      except BroadcastingError as e:
-       print("@%s<- failed" % upvote_comment.author)
+       print("Upvoting failed...")
+       print("We have probably reached the upvote rate limit.")
        print(str(e))
 
- 
+     if comment.author in my_favorites:
+       send_a_tip(comment.author)
+       print("====> Sent $0.001 SBD to @%s" % comment.author)
+
+
 if __name__ == "__main__":
     while True:
         try:
